8cd1817464a447ab509d2f7a25bc6b42feaef99a
Noah Lively <noah@koremedia.net>
2012-04-10 09:46:56 -0700

    Implemented ajax demo with custom-themed element

---
+++ templates/formtest-test-form.tpl.php
@@ -0,0 +1,23 @@
+<?php
+global $user;
+$items = $variables['my_form'];
+
+
+print $variables['my_form']['hidden'];
+?>
+
+<div id="my-form-outer">
+  
+  <?php print $variables['my_form']['item_1']; ?>
+  
+  <?php print $variables['my_form']['item_2']; ?>
+  
+  <hr />
+  
+  <?php print $variables['my_form']['my_container']; ?>
+  
+  <hr />
+  
+  <?php print $variables['my_form']['submit']; ?>
+  
+</div>
---
+++ templates/my-container-custom.tpl.php
@@ -0,0 +1,3 @@
+<div style="background: #ccccff; padding:20px;">
+<?php print $variables['element_rendered']; ?>
+</div>
